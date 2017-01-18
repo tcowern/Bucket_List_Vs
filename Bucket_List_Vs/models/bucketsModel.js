b2c5e80e7ea0d078c390cc355cd95028c6e8756c
tcowern <tcowern@yahoo.com>
2017-01-18 16:28:55 -0500

    Bucket List Version 2

---
+++ Bucket_List_Vs/models/bucketsModel.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+ bucketSchema = new mongoose.Schema({
+    userid: {type: mongoose.Schema.ObjectId, ref: "User"},
+    bucketname:{type : String, default: ""},
+    bucketdate : {type : String, default: "01/01/2017"},
+    buckets : {type : Array, default : []},
+    bucketcomp : {type : Boolean, default : false},
+    bucketcompdate: {type: String, default: ""},
+    topgroup: {type: Boolean, default: false},
+    bucketdays: {type: String, default: ""}
+
+});
+
+module.exports = mongoose.model('Bucket', bucketSchema);
+
+// db will be buckets
